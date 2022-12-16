a54a077b0520f7ba1676980dd72077d2dfa70171
jcoolbeing <jcoolbeing@gmail.com>
2022-12-16 16:37:12 +0000

    added some things

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+// ejs view engine
+let ejs = require('ejs');
+app.set('view engine', 'ejs');
 
+// routes
 app.get('/', (req, res) => {
   res.send('Employees')
+})
+
+app.get('/employees', (req, res)=>{
+  res.send('employees')
 })
 
 app.listen(port, () => {
